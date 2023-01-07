624e2f503731964dd5b98858d7a8b199f8c7b47d
CEHUB3 <erikhubbe@gmail.com>
2023-01-07 23:15:07 +0100

    commit

--- src/MaxDistanceFinder.java
+++ src/MaxDistanceFinder.java
@@ -1,7 +1,11 @@
 //Skriv metoden i denna klass
-//public class MaxDistanceFinder {
-//
-//    public int maxDistance(Place [] array){
-//
-//    }
-//}
+public class MaxDistanceFinder {
+
+    public double maxDistance(Place [] arrayInstans){
+
+        for (int i=0; i<arrayInstans.length; i=i+1){
+            arrayInstans[i].distanceTo(arrayInstans[]
+        }
+
+    }
+}
