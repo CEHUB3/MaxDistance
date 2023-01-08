bd048016ba76d775a7e95ec885952b44c42854ac
CEHUB3 <119689765+CEHUB3@users.noreply.github.com>
2023-01-08 21:45:18 +0100

    commit

--- src/MaxDistanceFinder.java
+++ src/MaxDistanceFinder.java
@@ -1,17 +1,18 @@
 //Skriv metoden i denna klass
 public class MaxDistanceFinder {
 
-    public double maxDistance(Place [] arrayInstans){
+    public static double maxDistance(Place[] arrayInstans) {
 
-        int comparedIndex = 1;
-        for (int i=0; i<arrayInstans.length; i=i+1){
-
-            double distancePP = arrayInstans[i].distanceTo(arrayInstans[i+comparedIndex];
-
-            comparedIndex = comparedIndex+1;
-
-
+        double maxDistance = 0;
+        for (int i = 0; i < arrayInstans.length; i++) {
+            for (int j = i + 1; j < arrayInstans.length; j++) {
+                double distancePP = arrayInstans[i].distanceTo(arrayInstans[j]);
+                if (distancePP > maxDistance) {
+                    maxDistance = distancePP;
+                }
+            }
         }
+        return maxDistance;
 
     }
 }
--- src/TestProgram.java
+++ src/TestProgram.java
@@ -11,16 +11,16 @@ public class TestProgram {
 		System.out.println("**********************************************************");
 		System.out.println();
 
-		Place p1 = new Place("Origo", 0, 0);
-		Place p2 = new Place("Utopia", 3, 4);
+		Place p1 = new Place("Origo", 86, 52);
+		Place p2 = new Place("Utopia", 49, 62);
 		Place p3 = new Place("Mars", 5, 7);
-		Place p4 = new Place("Mars", 6, 8);
-		Place p5 = new Place("Mars", 2, 8);
-		Place p6 = new Place("Mars", 11, 5);
-		Place p7 = new Place("Mars", 4, 7);
-		Place p8 = new Place("Mars", 3, 1);
+		Place p4 = new Place("Pluto", 6, 8);
+		Place p5 = new Place("Merkurius", 2, 8);
+		Place p6 = new Place("Venus", 11, 5);
+		Place p7 = new Place("Jupiter", 4, 7);
+		Place p8 = new Place("Neptunus", 3, 1);
 
-		Place[] placeList = new Place[3] ;
+		Place[] placeList = new Place[8] ;
 
 		placeList[0] = p1;
 		placeList[1] = p2;
@@ -33,14 +33,19 @@ public class TestProgram {
 
 		double distanceP1P2 = p1.distanceTo(p2);
 		System.out.println(distanceP1P2);
-		double distanceP1P3 = p1.distanceTo(p3);
-		System.out.println(distanceP1P3);
-		double distanceP2P3 = p2.distanceTo(p3);
-		System.out.println(distanceP2P3);
+//		double distanceP1P3 = p1.distanceTo(p3);
+//		System.out.println(distanceP1P3);
+//		double distanceP2P3 = p2.distanceTo(p3);
+//		System.out.println(distanceP2P3);
 
 
-		//public int maxDistance(placeList){
+//		double maxDist =  MaxDistanceFinder.maxDistance(placeList);
+//		MaxDistanceFinder maxDistFinder = new MaxDistanceFinder();
+//		double maxDist =  maxDistFinder.maxDistance(placeList);
 
+		double maxDist = MaxDistanceFinder.maxDistance(placeList);
+
+		System.out.println(maxDist);
 
 		}
 
